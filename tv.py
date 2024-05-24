78ae3b78042793a6c8538541995d2921518cca76
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-05-24 05:10:29 -0400

    Added a method for changing the channel

--- tv.py
+++ tv.py
@@ -1,27 +1,31 @@
 class tv:
 #add a data for instance variables
     def __init__(self):
-        #set the deffaukt settings
+        #set the deffault settings
         self.channel = 1
         self.volume = 1
         self.tv_on = False
-#add a method for power on
+#method for power on
     def turn_on(self):
         self.tv_on = True
-#add a method for power off
+#method for power off
     def turn_off(self):
         self.tv_on = False
-#add a method for getting the channel
+#method for getting the channel
     def get_channel(self):
         return self.channel
 #method for setting the channel to 1-120
     def set_channel(self,channel):
         if self.tv_on and 1 <= channel <= 120:
             self.channel = channel
-#add a method for getting the volume
+#method for getting the volume
     def get_volume(self):
         return self.volume
-#add a method for setting the volume to 1-7
+#method for setting the volume to 1-7
     def set_volume(self, volume):
         if self.tv_on and 1 <= volume <= 7:
             self.volume = volume
+#method for increasing the channel by 1 if the tv is on and if the current channel is less than 120
+    def channel_up(self):
+        if self.tv_on and self.channel < 120:
+            self.channel += 1
