9620ecbf8ae65ab97842562fb0f680b3e4f84065
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-05-24 05:29:18 -0400

    Created a two instances of the tv class

--- tv_class_import.py
+++ tv_class_import.py
@@ -1,4 +1,9 @@
 #add a method for importing class variables
 from tv import tv
+def main():
+    #instances of tv class
+    tv1 = tv()
+    tv2 = tv()
+    main()
 #add a operation for tv 1
 #add a operation for tv 2
