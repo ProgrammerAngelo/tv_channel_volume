c5dab2e9587e45da2a1bff6f423ff40cbdf0fb20
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-05-24 05:32:54 -0400

    Added an operation for tv1

--- tv_class_import.py
+++ tv_class_import.py
@@ -4,6 +4,9 @@ def main():
     #instances of tv class
     tv1 = tv()
     tv2 = tv()
+#add a operation for tv1
+    tv1.turn_on() #for turning on the tv
+    tv1.set_channel(30) #for setting the channel to channel 30
+    tv1.set_volume(3) #for setting the volume to volume 3
     main()
-#add a operation for tv 1
-#add a operation for tv 2
+#add a operation for tv2
