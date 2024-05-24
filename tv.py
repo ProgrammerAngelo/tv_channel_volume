51deee17c2a70733d65c84ce518a213acd1e0546
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-05-24 04:32:17 -0400

    Added a method to set the deffault settings for tv

--- tv.py
+++ tv.py
@@ -1,4 +1,10 @@
+class tv:
 #add a data for instance variables
+    def __init__(self):
+        #set the deffaukt settings
+        self.channel = 1
+        self.volume = 1
+        self.tv_on = False
 #add a method for power on
 #add a method for power off
 #add a method for getting the channel
